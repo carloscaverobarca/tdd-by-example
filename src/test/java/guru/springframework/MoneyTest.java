7c95922d9d1e0dd4efb128b5b104b3a38700d3ae
Carlos Cavero <cavero.carlos@gmail.com>
2021-04-08 18:08:06 +0200

    Add Franc support

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
-    void testMultiplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(new Dollar(10), product);
@@ -16,8 +16,23 @@ public class MoneyTest {
         assertEquals(new Dollar(15), product);
     }
 
-    @Test void testEquality() {
+    @Test void testEqualityDollar() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
+    }
+
+    @Test
+    void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test void testEqualityFranc() {
+        assertEquals(new Franc(5), new Franc(5));
+        assertNotEquals(new Franc(5), new Franc(6));
     }
 }

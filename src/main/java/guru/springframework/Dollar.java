58ad81aa2b9ca33279cc6b77361e73ddecf2eb2a
Carlos Cavero <cavero.carlos@gmail.com>
2021-04-08 17:56:53 +0200

    Add equality of objects

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,5 +1,7 @@
 package guru.springframework;
 
+import java.util.Objects;
+
 public class Dollar {
     public int amount;
 
@@ -9,5 +11,20 @@ public class Dollar {
 
     public Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
